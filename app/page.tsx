1048b0eff19b6ec6097999734a3b8b6c072a29c5
laryhills <hilaryokoh@gmail.com>
2025-09-15 23:37:39 +0100

    feat: add QRCodeModalExample component and update dependencies for QR code generation

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { PowerfulTools } from "./newComponents/powerful-tools";
 import { FAQSection } from "./newComponents/faq-section";
 import { HostInPeace } from "./newComponents/host-in-peace";
 import { TrendingNews } from "./newComponents/trending-news";
+import { QRCodeModalExample } from "./components/QRCodeModalExample";
 // import { Footer } from "@/components/footer"
 export default function HomePage() {
   return (
@@ -21,6 +22,11 @@ export default function HomePage() {
       <FAQSection />
       <HostInPeace />
       <TrendingNews />
+      
+      {/* QR Code Modal Test - Remove after testing */}
+      <div className="py-10">
+        <QRCodeModalExample />
+      </div>
     </div>
   );
 }
