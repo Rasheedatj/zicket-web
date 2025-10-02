09ceabaf2245fdafdb2e648cb46f5f1d032d87c6
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2025-10-02 10:37:02 +0100

    chore: import activitiesPanel in dashboard.tsx closes #51

--- app/(organizer)/zkorg/dashboard/page.tsx
+++ app/(organizer)/zkorg/dashboard/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavBar from '@/app/components/organizer/NavBar';
-import Summary from "@/app/components/organizer/analytics/Summary";
+import Summary from '@/app/components/organizer/analytics/Summary';
+import ActivitiesPanel from '../../../components/organizer/ActivitiesPanel';
 
 export default function OrgBoard() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className='min-h-screen bg-gray-50'>
       <NavBar />
       <Summary />
-      
+      <ActivitiesPanel />
     </div>
   );
 }
